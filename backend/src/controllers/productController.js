65fd4dadb390e065c79d0bd71268b11c40e79974
Addiel Juarez <addieljuarez@gmail.com>
2025-10-14 21:04:33 -0600

    feature/add controller and add to routes

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,32 @@
+class ProductController {
+  // clases static para acceder sin una instancia
+  static getAllProducts (req, res) {
+    res.json({
+      product: {}
+    })
+  }
+
+  static createProduct (req, res) {
+    res.json({
+      product: {}
+    })
+  }
+
+  static updateProduct (req, res) {
+    const { id } = req.params
+    res.json({
+      product: {},
+      id
+    })
+  }
+
+  static deleteProduct (req, res) {
+    const { id } = req.params
+    res.json({
+      product: {},
+      id
+    })
+  }
+}
+
+module.exports = ProductController
--- backend/src/routes/ProductRoutes.js
+++ backend/src/routes/ProductRoutes.js
@@ -1,20 +1,11 @@
 const express = require('express')
+const ProductController = require('../controllers/productController')
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('List of products')
-})
-
-router.post('/', (req, res) => {
-  res.send('Create a product')
-})
-
-router.put('/:id', (req, res) => {
-  res.send(`Update product with ID ${req.params.id}`)
-})
-
-router.delete('/:id', (req, res) => {
-  res.send(`Delete product with ID ${req.params.id}`)
-})
+router.get('/', ProductController.getAllProducts)
+router.post('/', ProductController.createProduct)
+router.put('/:id', ProductController.updateProduct)
+router.delete('/:id', ProductController.deleteProduct)
 
 module.exports = router
