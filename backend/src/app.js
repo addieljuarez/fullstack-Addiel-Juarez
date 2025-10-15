a27a338b0e26473cfa60ef223665bb213936c59c
Addiel Juarez <addieljuarez@gmail.com>
2025-10-14 20:52:24 -0600

    feature/add error handle

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const productRoutes = require('./routes/productRoutes')
+const errorHandler = require('./middlewares/errorHandler')
 
 // crear express app
 const app = express()
@@ -13,5 +14,8 @@ app.use(helmet()) // seguridad extra en las headers de la peticion
 
 // Routes
 app.use('/products', productRoutes)
+
+// middleware error
+app.use(errorHandler)
 
 module.exports = app
